3ae28908bdd600c8464536e4bdb4d2bf3bc70888
Fabrício Dias <dias.fabricio0812@gmail.com>
2025-05-05 20:06:27 -0300

    Buscando comentários

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,9 +1,10 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { useSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import styles from "./styles.module.css";
-import { GetServerSideProps } from "next";
-import { db } from "@/services/firebaseConnection";
+
+import { db } from "../../services/firebaseConnection";
 import {
   doc,
   collection,
@@ -11,49 +12,62 @@ import {
   where,
   getDoc,
   addDoc,
+  getDocs,
 } from "firebase/firestore";
-import { Textarea } from "@/components/textarea";
+
+import { Textarea } from "../../components/textarea";
 
 interface TaskProps {
   item: {
     tarefa: string;
-    public: boolean;
     created: string;
+    public: boolean;
     user: string;
     taskId: string;
   };
+  allComments: CommentProps[];
 }
 
-export default function Task({ item }: TaskProps) {
-  const { data: session } = useSession();
-  const [input, setInput] = useState("");
+interface CommentProps {
+  id: string;
+  comment: string;
+  taskId: string;
+  user: string;
+  name: string;
+}
 
-  async function handleComment(e: FormEvent) {
-    e.preventDefault();
+export default function Task({ item, allComments }: TaskProps) {
+  const { data: session } = useSession();
+
+  const [input, setInput] = useState("");
+  const [comments, setComments] = useState<CommentProps[]>(allComments || []);
+
+  async function handleComment(event: FormEvent) {
+    event.preventDefault();
 
     if (input === "") return;
 
     if (!session?.user?.email || !session?.user?.name) return;
 
     try {
-      await addDoc(collection(db, "comments"), {
+      const docRef = await addDoc(collection(db, "comments"), {
         comment: input,
         created: new Date(),
         user: session?.user?.email,
         name: session?.user?.name,
-        tarefa: item?.taskId,
+        taskId: item?.taskId,
       });
 
       setInput("");
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
     }
   }
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Tarefas+ | Detalhes da tarefa</title>
+        <title>Tarefa - Detalhes da tarefa</title>
       </Head>
 
       <main className={styles.main}>
@@ -65,22 +79,32 @@ export default function Task({ item }: TaskProps) {
 
       <section className={styles.commentsContainer}>
         <h2>Deixar comentário</h2>
+
         <form onSubmit={handleComment}>
           <Textarea
-            placeholder="Digite seu comentário"
             value={input}
-            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-              setInput(e.target.value)
+            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+              setInput(event.target.value)
             }
+            placeholder="Digite seu comentário..."
           />
-          <button
-            type="submit"
-            disabled={!session?.user}
-            className={styles.button}
-          >
+          <button disabled={!session?.user} className={styles.button}>
             Enviar comentário
           </button>
         </form>
+      </section>
+
+      <section className={styles.commentsContainer}>
+        <h2>Todos comentários</h2>
+        {comments.length === 0 && (
+          <span>Nenhum comentário foi encontrado...</span>
+        )}
+
+        {comments.map((item) => (
+          <article key={item.id} className={styles.comment}>
+            <p>{item.comment}</p>
+          </article>
+        ))}
       </section>
     </div>
   );
@@ -88,9 +112,25 @@ export default function Task({ item }: TaskProps) {
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const id = params?.id as string;
+  const docRef = doc(db, "tarefas", id);
 
-  const snapshot = await getDoc(doc(db, "tarefas", id));
-  if (!snapshot.data()) {
+  const q = query(collection(db, "comments"), where("taskId", "==", id));
+  const snapshotComments = await getDocs(q);
+
+  let allComments: CommentProps[] = [];
+  snapshotComments.forEach((doc) => {
+    allComments.push({
+      id: doc.id,
+      comment: doc.data().comment,
+      user: doc.data().user,
+      name: doc.data().name,
+      taskId: doc.data().taskId,
+    });
+  });
+
+  const snapshot = await getDoc(docRef);
+
+  if (snapshot.data() === undefined) {
     return {
       redirect: {
         destination: "/",
@@ -109,6 +149,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
 
   const miliseconds = snapshot.data()?.created?.seconds * 1000;
+
   const task = {
     tarefa: snapshot.data()?.tarefa,
     public: snapshot.data()?.public,
@@ -120,6 +161,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   return {
     props: {
       item: task,
+      allComments: allComments,
     },
   };
 };
