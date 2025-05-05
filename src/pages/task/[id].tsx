8688a85e9127d8eab1437e9a16f8d3668cbcfb69
Fabrício Dias <dias.fabricio0812@gmail.com>
2025-05-05 20:16:26 -0300

    Exibindo comentários

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -3,7 +3,6 @@ import { useSession } from "next-auth/react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import styles from "./styles.module.css";
-
 import { db } from "../../services/firebaseConnection";
 import {
   doc,
@@ -14,8 +13,8 @@ import {
   addDoc,
   getDocs,
 } from "firebase/firestore";
-
 import { Textarea } from "../../components/textarea";
+import { FaTrash } from "react-icons/fa";
 
 interface TaskProps {
   item: {
@@ -58,6 +57,15 @@ export default function Task({ item, allComments }: TaskProps) {
         taskId: item?.taskId,
       });
 
+      const data = {
+        id: docRef.id,
+        comment: input,
+        user: session?.user?.email,
+        name: session?.user?.name,
+        taskId: item?.taskId,
+      };
+
+      setComments((prevComments) => [...prevComments, data]);
       setInput("");
     } catch (err) {
       console.log(err);
@@ -102,6 +110,14 @@ export default function Task({ item, allComments }: TaskProps) {
 
         {comments.map((item) => (
           <article key={item.id} className={styles.comment}>
+            <div className={styles.headComment}>
+              <label className={styles.commentsLabel}>{item.name}</label>
+              {item.user === session?.user?.email && (
+                <button className={styles.buttonTrash}>
+                  <FaTrash size={18} color="#ea3140" />
+                </button>
+              )}
+            </div>
             <p>{item.comment}</p>
           </article>
         ))}
