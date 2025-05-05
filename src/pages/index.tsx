996f94bd2302a903af172a9a1f9f5937b22b08b2
Fabrício Dias <dias.fabricio0812@gmail.com>
2025-05-05 09:09:18 -0300

    Criando o header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import heroImg from "../../public/assets/hero.png";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
@@ -23,6 +23,15 @@ export default function Home() {
         <h1 className={styles.title}>
           Sistema feito para você organizar <br /> seus estudos e tarefas
         </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
       </main>
     </div>
   );
