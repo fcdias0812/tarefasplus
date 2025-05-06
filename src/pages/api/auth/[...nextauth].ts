f98f4e0e36166f74e2f19ad77f6d383ef34ce093
Fabrício Dias <dias.fabricio0812@gmail.com>
2025-05-05 21:18:45 -0300

    Arrumando erros de módulo

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID! as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
     }),
   ],
   secret: process.env.JWT_SECRET as string,
